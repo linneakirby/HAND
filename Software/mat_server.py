173296ca1f2212bf504b6971b4a93059c0b2da94
Linnea Kirby <linneakirby@users.noreply.github.com>
2023-04-12 17:21:20 -0400

    attempt to update server

---
+++ Software/mat_server.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import pressure_mat_posture as pmp
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+
+@app.route('/helloarduino')
+def helloHandler():
+    mat.get_matrix()
+    fig, ax = plt.subplots(figsize=(5,5))
+    plt.ion()
+    p_dict = pmp.execute_instructions(mat.Values, fig)
+    plt.show()
+    return p_dict.get("a")
+    # return create_actuator_dict()
+
+def create_actuator_dict():
+    actuators = list()
+    actuators.append((True, .8)) #index
+    actuators.append((True, .4)) #pinky
+    actuators.append((False, 0.0)) #wrist
+    actuators.append((False, 0.0)) #thumb
+    return actuators
+
+def main():
+    global mat
+    mat = pmp.Mat(pmp.get_port())
+    app.run(host='0.0.0.0', port=8090)
+
+if __name__ == '__main__':
+    main()
