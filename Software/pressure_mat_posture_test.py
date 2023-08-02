23201022f0ee875efa80b7855eec7ea6e2d1ce77
Linnea Kirby <linneakirby@users.noreply.github.com>
2023-08-01 20:57:18 -0400

    add more tests

--- Software/pressure_mat_posture_test.py
+++ Software/pressure_mat_posture_test.py
@@ -61,6 +61,44 @@ class Pressure_Mat_Posture_Test(unittest.TestCase):
         self.assertEquals(r.get((1, 2)), 2.0)
         self.assertEquals(l.get((1, 0)), 1.0)
 
+    def test_calculate_cop(self):
+        weighted_coords = dict()
+        weighted_coords[(1.0, 2.0)] = 2.0
+        weighted_coords[(1.0, 0.0)] = 1.0
+
+        unweighted_coords = dict()
+        unweighted_coords[(1.0, 2.0)] = 1.0
+        unweighted_coords[(1.0, 0.0)] = 1.0
+
+        self.assertEquals(pmp.calculate_cop(weighted_coords),[1.0, 4.0/3.0])
+        self.assertEquals(pmp.calculate_cop(unweighted_coords), [1.0, 1.0])
+
+    #make sure it's doing the calculations properly on a simple example
+    def test_cop_calculations(self):
+        m = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 2.0], [0.0,0.0,0.0]])
+        kmeans, coords_only = pmp.run_kmeans(m, 2, 3, 3)
+        h1, h2 = pmp.isolate_hands(m, kmeans, coords_only)
+        actual_rcop, actual_lcop, ideal_cop, actual_cop, r, l = pmp.generate_cops(h1, h2)
+
+        self.assertEquals(actual_rcop, [1.0, 2.0])
+        self.assertEquals(actual_lcop, [1.0, 0.0])
+        self.assertEquals(actual_cop, [1.0, 4.0/3.0])
+        self.assertEquals(ideal_cop, [1.0, 1.0])
+
+    #probably doesn't need to be a test but w/e
+    def test_create_actuators(self):
+        a = pmp.create_actuator_dict()
+        test_a = dict()
+        test_a['i'] = False #index
+        test_a['p'] = False #pinky
+        test_a['w'] = False #wrist
+        test_a['t'] = False #thumb
+
+        self.assertIsInstance(a, dict)
+        self.assertEquals(a, test_a)
+
+    
+
 
 if __name__ == '__main__':
     unittest.main()
