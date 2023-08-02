4da9ccf2b4b421dab27b7b62f0bf1930a043c59a
Linnea Kirby <linneakirby@users.noreply.github.com>
2023-08-01 21:09:58 -0400

    add tests for vector calculation

--- Software/pressure_mat_posture_test.py
+++ Software/pressure_mat_posture_test.py
@@ -61,6 +61,7 @@ class Pressure_Mat_Posture_Test(unittest.TestCase):
         self.assertEquals(r.get((1, 2)), 2.0)
         self.assertEquals(l.get((1, 0)), 1.0)
 
+    #make sure the basic cop calculation function works
     def test_calculate_cop(self):
         weighted_coords = dict()
         weighted_coords[(1.0, 2.0)] = 2.0
@@ -73,7 +74,7 @@ class Pressure_Mat_Posture_Test(unittest.TestCase):
         self.assertEquals(pmp.calculate_cop(weighted_coords),[1.0, 4.0/3.0])
         self.assertEquals(pmp.calculate_cop(unweighted_coords), [1.0, 1.0])
 
-    #make sure it's doing the calculations properly on a simple example
+    #make sure it's doing all the calculations properly on a simple mat example
     def test_cop_calculations(self):
         m = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 2.0], [0.0,0.0,0.0]])
         kmeans, coords_only = pmp.run_kmeans(m, 2, 3, 3)
@@ -96,6 +97,32 @@ class Pressure_Mat_Posture_Test(unittest.TestCase):
 
         self.assertIsInstance(a, dict)
         self.assertEquals(a, test_a)
+
+    #make sure the basic vector calculation function works
+    def test_create_vector(self):
+        t1 = [1.0, 4.0/3.0]
+        t2 = [1.0, 1.0]
+        tv = [0.0, 0.0]
+        tv[0] = t2[0] - t1[0]
+        tv[1] = t2[1] - t1[1]
+
+        self.assertEquals(pmp.create_vector(t1, t2), (tv[0], tv[1]))
+
+    #make sure the vector is calculated properly using a simple mat example
+    def test_vector(self):
+        m = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 2.0], [0.0,0.0,0.0]])
+        kmeans, coords_only = pmp.run_kmeans(m, 2, 3, 3)
+        h1, h2 = pmp.isolate_hands(m, kmeans, coords_only)
+        actual_rcop, actual_lcop, ideal_cop, actual_cop, r, l = pmp.generate_cops(h1, h2)
+        vector = pmp.create_vector(actual_cop, ideal_cop)
+
+        t1 = [1.0, 4.0/3.0]
+        t2 = [1.0, 1.0]
+        tv = [0.0, 0.0]
+        tv[0] = t2[0] - t1[0]
+        tv[1] = t2[1] - t1[1]
+
+        self.assertEquals(vector, (tv[0], tv[1]))
 
     
 
